4ee4182fe365abd0f909b8e04d6dfeb355e354a6
ChenWang <1401520906@qq.com>
2022-09-28 18:34:15 +0800

    add spacemacs

---
+++ .emacs.d/init.el
@@ -0,0 +1,5 @@
+;; set variable `spacemacs-start-directory' to point to your Spacemacs
+;; installation location, "~" is equivalent to "$HOME"
+(setq spacemacs-start-directory "~/.emacs.d/spacemacs/")
+;; load Spacemacs's initialization file
+(load-file (expand-file-name "init.el" spacemacs-start-directory))
