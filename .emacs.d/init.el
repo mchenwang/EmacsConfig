bca64d242931eefce69b95ef3864d81f939bcaa2
ChenWang <1401520906@qq.com>
2022-09-29 10:39:13 +0800

    set E_HOME

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,7 +2,7 @@
 (load-file "~/.emacs.d/init-window.el")
 
 ;; set variable `spacemacs-start-directory' to point to your Spacemacs
-;; installation location, "~" is equivalent to "$HOME"
+;; installation location, "~" is equivalent to "$E_HOME"
 (setq spacemacs-start-directory "~/.emacs.d/spacemacs/")
 ;; load Spacemacs's initialization file
 (load-file (expand-file-name "init.el" spacemacs-start-directory))
